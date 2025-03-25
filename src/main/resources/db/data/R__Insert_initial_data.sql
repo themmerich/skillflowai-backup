720e749743d5c9466ba1f0bf5011b13dab55e9df
Thomas Hemmerich <t.hemmerich.extern@drgueldener.de>
2025-03-25 07:49:07 +0100

    minor changes

--- src/main/resources/db/data/R__Insert_initial_data.sql
+++ src/main/resources/db/data/R__Insert_initial_data.sql
@@ -7,3 +7,30 @@ INSERT INTO skillflowai.user_roles (user_id, role_id) VALUES ( 2, 2);
 INSERT INTO skillflowai.organization
 (created_by_id, created_date, id, last_modified_by_id, last_modified_date, description, "name")
 VALUES(1, current_timestamp, 0, 1, current_timestamp, 'some desc...', 'Feuerwehr Grafenrheinfeld');
+INSERT INTO skillflowai.organization
+(created_by_id, created_date, id, last_modified_by_id, last_modified_date, description, "name")
+VALUES(1, current_timestamp, 1, 1, current_timestamp, 'some more desc...', 'Feuerwehr Bamberg');
+INSERT INTO skillflowai.organization
+(created_by_id, created_date, id, last_modified_by_id, last_modified_date, description, "name")
+VALUES(1, current_timestamp, 2, 1, current_timestamp, 'some nice desc...', 'Feuerwehr Würzburg');
+INSERT INTO skillflowai.organization
+(created_by_id, created_date, id, last_modified_by_id, last_modified_date, description, "name")
+VALUES(1, current_timestamp, 3, 1, current_timestamp, 'some hot desc...', 'Feuerwehr Kitzingen');
+INSERT INTO skillflowai.organization
+(created_by_id, created_date, id, last_modified_by_id, last_modified_date, description, "name")
+VALUES(1, current_timestamp, 4, 1, current_timestamp, 'some blurry desc...', 'Feuerwehr Volkach');
+INSERT INTO skillflowai.organization
+(created_by_id, created_date, id, last_modified_by_id, last_modified_date, description, "name")
+VALUES(1, current_timestamp, 5, 1, current_timestamp, 'some flurry desc...', 'Feuerwehr Bad Kissingen');
+INSERT INTO skillflowai.organization
+(created_by_id, created_date, id, last_modified_by_id, last_modified_date, description, "name")
+VALUES(1, current_timestamp, 6, 1, current_timestamp, 'some icy desc...', 'Feuerwehr Hammelburg');
+INSERT INTO skillflowai.organization
+(created_by_id, created_date, id, last_modified_by_id, last_modified_date, description, "name")
+VALUES(1, current_timestamp, 7, 1, current_timestamp, 'some shitty desc...', 'Feuerwehr Schweinfurt');
+INSERT INTO skillflowai.organization
+(created_by_id, created_date, id, last_modified_by_id, last_modified_date, description, "name")
+VALUES(1, current_timestamp, 8, 1, current_timestamp, 'some strange desc...', 'Feuerwehr Grettstatt');
+INSERT INTO skillflowai.organization
+(created_by_id, created_date, id, last_modified_by_id, last_modified_date, description, "name")
+VALUES(1, current_timestamp, 9, 1, current_timestamp, 'some nice desc...', 'Feuerwehr Bergrheinfeld');
